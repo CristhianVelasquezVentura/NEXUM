61d1ab685b075585ab52af5c27de3a4b8c10b877
Marklnz1 <jhon4lnz@gmail.com>
2023-08-25 19:46:19 -0500

    update: add workflow services and component

---
+++ src/app/core/services/worklflow/workflow.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  DtoWorkflow,
+  ResponseAttachedDocument,
+  ResponseEmailNoticeToSigner,
+  ResponseGetRolesSigners,
+  ResponseGetUserById,
+  ResponseGetWorkflow, ResponseNotificationEmail,
+  ResponseNotificationSms,
+  ResponseOtp,
+  ResponseOtpNotificationEmail,
+  ResponseOtpNotificationSms,
+  ResponseRequestedDocument,
+  ResponseSignatureAppearance,
+  ResponseSmsNoticeToSigner,
+  ResponseWorkflow
+} from "@app/modules/workflow/models/steps";
+import {EnvServiceProvider} from "@app/core/services/env/env.service.provider";
+import { Observable } from 'rxjs';
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkflowService {
+  private urlCreateWorkflow = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/workflow';
+  private urlGetWorkflow = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/workflow/10/0';
+  private urlGetSignatureAppearance = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/signature_appearance/workflow';
+  private urlGetRolesSigners = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/roles/signer/workflow';
+  private urlEmailNoticeSigner = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/email/notice/signer';
+  private urlSmsNoticeSigner = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/sms/notice/signer';
+  private urlOtp = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/otp';
+  private urlOtpNotificationsSms = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/otp-notifications/sms';
+  private urlOtpNotificationsEmail = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/notification/otp/email';
+  private urlAttachedDocument = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/attached/document';
+  private urlRequestedDocument = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/requested/document';
+  private urlNotificationSms = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/notification/sms';
+  private urlNotificationEmail = EnvServiceProvider.useFactory().API_WORKFLOW + '/api/v1/notification/email';
+  constructor(    private _http: HttpClient
+    ) { 
+  }
+  public getWorkflows(): Observable<ResponseGetWorkflow> {
+    return this._http.get<ResponseGetWorkflow>(this.urlGetWorkflow).pipe(map(res => res));
+  }
+  
+}
