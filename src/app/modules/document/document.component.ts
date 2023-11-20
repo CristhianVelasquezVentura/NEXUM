457181d1382464dfeeba45867c190e1f1fa135b6
cristhian.velasquez <cristhian.velasquez@e-capture.io>
2023-11-19 21:17:33 -0500

    fix module document how standalone

--- src/app/modules/document/document.component.ts
+++ src/app/modules/document/document.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-document',
   templateUrl: './document.component.html',
-  styleUrls: ['./document.component.scss']
+  styleUrls: ['./document.component.scss'],
+  standalone: true
 })
 export class DocumentComponent implements OnInit {
 
